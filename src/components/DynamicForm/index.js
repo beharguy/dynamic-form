b825355a56476db8b3c7b28a0d62381b49770f8a
Behar Guy <beharguy@gmail.com>
2020-05-16 17:57:38 +0300

    Prepering for npm package

--- src/components/DynamicForm/index.js
+++ src/components/DynamicForm/index.js
@@ -1,5 +1,7 @@
 import DynamicForm from './DynamicForm';
 import * as types from './consts/types';
+import * as utils from '../../utils';
 
 export default DynamicForm;
 export { types };
+export { utils };
