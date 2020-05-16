51ec5437c3a5581ca968287e11877d35eb1ca129
Behar Guy <beharguy@gmail.com>
2020-05-16 16:45:25 +0300

    column and row gaps added

--- src/components/StyledComponents/StyledForm.js
+++ src/components/StyledComponents/StyledForm.js
@@ -3,6 +3,8 @@ import styled, { css } from 'styled-components'
 export default styled.div`
   display: grid;
   grid-template-rows: auto;
+  column-gap: 10px;
+  row-gap: 10px;
 
   ${props => props.gridTemplateColumns ? css`
     grid-template-columns: ${props.gridTemplateColumns};
